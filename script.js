62ba702f533f9dd6c5923d87a555ea93d80cca87
Daniel Correia <130774958+DanielJavaprogrammer@users.noreply.github.com>
2025-03-06 16:49:46 -0300

    ok

---
+++ script.js
@@ -0,0 +1,7 @@
+var httpRequest;
+
+if (window.XMLHttpRequest) { // Mozilla, Safari, ...
+    httpRequest = new XMLHttpRequest();
+} else if (window.ActiveXObject) { // IE 8 and older
+    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
+}
