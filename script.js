044f0c33afc69cdd615cc309769b958a2acca2f4
Daniel Sodré <130774958+DanielJavaprogrammer@users.noreply.github.com>
2025-03-06 21:59:46 -0300

    06-03

--- script.js
+++
@@ -1,7 +0,0 @@
-var httpRequest;
-
-if (window.XMLHttpRequest) { // Mozilla, Safari, ...
-    httpRequest = new XMLHttpRequest();
-} else if (window.ActiveXObject) { // IE 8 and older
-    httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-}
---
+++ teste.php
@@ -0,0 +1,4 @@
+<?php
+$data = ["mensagem" => "AJAX funcionando!"];
+echo json_encode($data);
+?>
