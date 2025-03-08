8d45c84936c37f1ffa3e16e4219522e6fb4b8f4f
Daniel Correia <130774958+DanielJavaprogrammer@users.noreply.github.com>
2025-03-08 07:51:30 -0300

    ok

--- config/database.php
+++ config/database.php
@@ -1,6 +1,16 @@
 <?php
 
-define('DATA_HOST', 'localhost');
-define('DATA_USER', 'root');
-define()
-define()
+$host = "localhost";
+$usuario = "estoque_user_daniel";
+$senha = "senha_segura_1998";
+$banco_dados = "estoque_redhouse";
+
+try {
+    // 1️⃣ Criando a conexão PDO
+    $pdo = new PDO("mysql:host=$host;dbname=$banco_dados;charset=utf8", $usuario, $senha);
+    // 2️⃣Configurar o PDO para lançar exceções em caso de erro
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // 3️⃣ Capturar erro e exibir mensagem personalizada
+    die("Erro na conexao" . $e->getMessage());
+}
---
+++ public/login.php
@@ -0,0 +1,22 @@
+<?php
+require "../includes/header.php";
+require "../includes/footer.php";
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Gerenciador de estoque exlusivo Red House.">
+    <meta name="keywords" content="estoque, gerenciamento, controle, produtos, inventário">
+    <link rel="stylesheet" href="..\public\css\login.css">
+    <title>Estoque</title>
+</head>
+
+<body>
+    <h1>Hello World</h1>
+</body>
+
+</html>
